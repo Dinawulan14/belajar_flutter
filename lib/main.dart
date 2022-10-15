d104880a70fb2679a884f2de12ed601ae10517df
Dina Wulan <dina.wulan.ft21@mail.umy.ac.id>
2022-10-15 21:08:09 +0700

    menambah widget Aplikasiku

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const Aplikasiku());
+}
+
+class Aplikasiku extends StatelessWidget {
+  const Aplikasiku({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
